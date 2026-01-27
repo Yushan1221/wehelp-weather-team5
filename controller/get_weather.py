5e83b33de12b143189a547733762c1ec7761b2d2
kaun <kuan@gmail.com>
2026-01-27 14:20:17 +0800

    fix bug

--- controller/get_weather.py
+++ controller/get_weather.py
@@ -1,14 +1,13 @@
 from fastapi import *
-from models.temp_api import *
-from models.weather_api import *
+from models import temp_api, weather_api
+
 router = APIRouter()
 
 @router.get("/api/weather")
 def weather():
-    return get_weather()
+    return weather_api.get_weather()
 
 @router.get("/api/temp")
 def tmep():
-    return get_tmep()
+    return temp_api.get_tmep()
 
-    
--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from fastapi import *
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
-from mysql.connector import Error
+from controller import get_weather
 from dotenv import load_dotenv
 load_dotenv()
 
 app=FastAPI()
 
 app.mount("/static", StaticFiles(dictionary="static"))
+
+app.include_router(get_weather.router)
 
 @app.get("/", include_in_schema=False)
 async def index(request: Request):
